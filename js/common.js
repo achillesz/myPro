a94f222b2956920e7e42af3541c1001c581ccbd6
wang zhiyong <36891314@163.com>
2013-04-28 15:05:09 +0800

    reset style

--- js/common.js
+++ js/common.js
@@ -5,3 +5,52 @@
  * Time: ÉÏÎç10:18
  * To change this template use File | Settings | File Templates.
  */
+// ¼Ì³Ð
+function Class(parent) {
+    var kclass = function () {
+        if (parent) {
+            parent.apply(this, arguments);
+        }
+        this.init.apply(this, arguments);
+    }
+    if (parent) {
+        kclass.prototype = new parent;
+    }
+    kclass.prototype.init = function () {
+    };
+    kclass.fn = kclass.prototype;
+    kclass.fn.parent = kclass;
+    kclass.extend = function (obj) {
+        var extended = obj.extended;
+        for (i in obj) {
+            if (!kclass[i]) {
+                kclass[i] = obj[i];
+            }
+        }
+        if (extended) extended(kclass);
+    }
+    kclass.include = function (obj) {
+        var included = obj.included;
+        for (i in obj) {
+            kclass.fn[i] = obj[i];
+        }
+        if (included) {
+            included(kclass);
+        }
+        return kclass;
+    }
+    kclass.proxy = function (func) {
+        var self = this;
+        return(function () {
+            return func.apply(self, arguments);
+        });
+    }
+    return kclass;
+}
+
+// Add Class Drag
+var Drag = Class();
+Drag.fn.init = function(){
+    this.dragEle = dragEle;
+};
+var dragA = new Drag();
